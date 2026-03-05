edd0e3465ee9a78e3fe68ca54100e9f8f7d17096
Moker32 <tshx1992@hotmail.com>
2026-03-05 09:38:36 +0800

    feat(frontend): 添加 Element Plus UI 组件库

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 import App from './App.vue'
 import '@/styles/main.scss'
@@ -12,5 +14,6 @@ pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
