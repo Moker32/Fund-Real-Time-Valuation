e9f3f9f739d01647636fe8abad5821191625d517
Moker32 <tshx1992@hotmail.com>
2026-04-20 15:20:43 +0800

    fix: 美股指数折线图午休压缩逻辑，dow_jones/nasdaq/sp500 使用无午休配置

--- web/src/components/IndexCard.vue
+++ web/src/components/IndexCard.vue
@@ -110,7 +110,7 @@ const emit = defineEmits<{
 // eslint-disable-next-line no-useless-assignment
 const indexData = computed(() => props.index);
 
-// 午休配置：A股 11:30-13:00，港股 12:00-13:00，日经 12:30-13:30
+// 午休配置：A股 11:30-13:00，港股 12:00-13:00，日经 12:30-13:30，美股/其他无午休
 const lunchBreak = computed(() => {
   switch (props.index.index) {
     case 'nikkei225':
@@ -118,6 +118,10 @@ const lunchBreak = computed(() => {
     case 'hang_seng':
     case 'hang_seng_tech':
       return { start: 12 * 60, end: 13 * 60 }; // 12:00-13:00
+    case 'dow_jones':
+    case 'nasdaq':
+    case 'sp500':
+      return { start: 0, end: 0 }; // 美股无午休
     default:
       return { start: 11 * 60 + 30, end: 13 * 60 }; // A 股 11:30-13:00
   }
